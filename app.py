128cfdb46f97e9f725b3898bb6ce8440e4908587
Thomas Wigley <66223217+thomas-wigley@users.noreply.github.com>
2020-06-09 19:22:32 +0100

    added asset boilerplate

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
-from flask import Flask, request
-import git
+from flask import Flask, render_template, request
+from flask_assets import Environment, Bundle
+
+
+
 app = Flask(__name__)
+
+# the following preprocesses the scss files, converting them
+# into regular (as well as minified) css files in /static/gen
+assets = Environment(app)
+assets.register({
+    'home-css': Bundle(
+        'css/home.scss',
+        filters=('libsass', 'cssmin'),
+        depends='css/*.scss',
+        output='gen/home.%(version)s.min.css'),
+})
+
 
 @app.route('/')
 def home():
-    return 'Cool site coming soon'
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
